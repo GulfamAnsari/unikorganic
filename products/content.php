d8a4be6b30a4a64bbbee0bced70d44bc4b1d6b31
Gulfam <gulfam.ansari@elucidata.io>
2019-05-16 15:49:45 +0530

    add content.php for products

--- products/content.php
+++ products/content.php
@@ -1,9 +1,23 @@
 <!-- main body -->
 <div class="container">
-    <div class="col-md-9">
+    <div class="col-md-12" style="margin: 8px 0">
         <h2>Our Products</h2>
         <hr>
-    </div>
-    <div class="col-md-3">
+        <?php
+        $strJsonFileContents = file_get_contents("products.json");
+        // Convert to array 
+        $data = json_decode($strJsonFileContents, true);
+        foreach ($data as $obj) {
+            ?>
+            <div class="col-md-2 products">
+                <a href="/products">
+                    <img src="<?php echo ($obj['image']); ?>">
+                    <p><?php echo ($obj['name']); ?></p>
+                </a>
+            </div>
+        <?php
+    }
+    ?>
+        <hr>
     </div>
 </div>
