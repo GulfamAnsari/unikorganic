9a816a45e4550baf5e1638fc4129e47c993b3450
Gulfam <gulfam.ansari@elucidata.io>
2019-05-16 15:06:37 +0530

    prodyct page added

---
+++ products/content.php
@@ -0,0 +1,9 @@
+<!-- main body -->
+<div class="container">
+    <div class="col-md-9">
+        <h2>Our Products</h2>
+        <hr>
+    </div>
+    <div class="col-md-3">
+    </div>
+</div>
---
+++ products/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>UnikOrganic - Products</title>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/head.php'); ?>
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/nav.php'); ?>
+    <?php include('content.php'); ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
+</body>
+
+</html>
